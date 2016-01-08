61fc7a3d0a44072d2335fc08f7f888165f459e51
Turtle-Corder <is7khk10@naver.com>
2016-01-09 07:04:43 +0900

    v0.0.2   -Top-Down Base sample Loaded

---
+++ Forge/Source/Forge/Forge.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "Forge.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Forge, "Forge" );
+
+DEFINE_LOG_CATEGORY(LogForge)
+ 
---
+++ Forge/Source/Forge/Forge.h
@@ -0,0 +1,11 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __FORGE_H__
+#define __FORGE_H__
+
+#include "EngineMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogForge, Log, All);
+
+
+#endif
